61131edb6a70bab6b89803513214cbe2027c2201
Linus Miller <lohfu@lohfu.io>
2017-04-24 14:35:01 +0200

    Better config verification

--- handlers.js
+++ handlers.js
@@ -5,7 +5,11 @@ const factory = require('midwest/factories/handlers');
 
 const columns = ['id', 'givenName', 'familyName', 'telephone', 'email', 'jobTitle', 'image', 'dateCreated', 'createdById', 'dateModified'];
 
+const config = require('./config');
+
 const handlers = factory({
+  db: config.db,
+  emitter: config.emitter,
   table: 'employees',
   columns,
 });
