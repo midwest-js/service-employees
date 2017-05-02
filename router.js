b0379fbb98f8a04afc2200c57a9aa8dcd444fd32
Linus Miller <lohfu@lohfu.io>
2017-05-02 15:17:34 +0200

    Split midwest utils into modules and remove auth

--- router.js
+++ router.js
@@ -4,16 +4,14 @@ const router = new (require('express')).Router();
 
 const mw = require('./middleware');
 
-const { isAdmin } = require('midwest-module-membership/passport/authorization-middleware');
-
 router.route('/')
   .get(mw.formatQuery, mw.paginate, mw.find)
-  .post(isAdmin, mw.create);
+  .post(mw.create);
 
 router.route('/:id')
   .get(mw.findById)
-  .patch(isAdmin, mw.update)
-  .put(isAdmin, mw.replace)
-  .delete(isAdmin, mw.remove);
+  .patch(mw.update)
+  .put(mw.replace)
+  .delete(mw.remove);
 
 module.exports = router;
