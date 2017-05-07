bf4e7a4c47b924a402b43c0a80f03fd86a8516e4
Linus Miller <lohfu@lohfu.io>
2017-05-08 01:14:39 +0200

    Sort by ID asc

--- handlers.js
+++ handlers.js
@@ -3,6 +3,9 @@
 // modules > 3rd party
 const _ = require('lodash');
 
+const sql = require('easy-postgres/sql-helpers');
+const { many } = require('easy-postgres/result');
+
 // modules > midwest
 const factory = require('midwest/factories/handlers');
 const resolveCache = require('./resolve-cache');
@@ -21,13 +24,22 @@ const columns = [
   'modifiedById',
 ];
 
+const columnsString = sql.columns(columns);
+
 module.exports = _.memoize((config) => {
+  function getAll(client = config.db) {
+    const query = `SELECT ${columnsString} FROM employees ORDER BY id ASC;`;
+
+    return client.query(query).then(many);
+  }
+
   const handlers = factory({
     db: config.db,
     emitter: config.emitter,
+    exclude: ['getAll'],
     table: 'employees',
     columns,
   });
 
-  return handlers;
+  return Object.assign(handlers, { getAll });
 }, resolveCache);
